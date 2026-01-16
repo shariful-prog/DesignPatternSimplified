98caf8e5fa2e59503dca295b246b88ac6daf8abc
shariful.islam <shariful.islam@numberskills.se>
2026-01-16 12:16:00 +0600

    Added Factory pattern with Adapter to simplified object creation

--- MainApp/PatternDemo/AdapterPatternDemo.cs
+++ MainApp/PatternDemo/AdapterPatternDemo.cs
@@ -10,16 +10,26 @@ internal class AdapterPatternDemo : IPatternDemo
     public async Task RunAsync()
     {
         Console.WriteLine("Starting Structural Pattern - Adapter");
+        var paymentRequest = new PaymentRequest("4111111111111111", 100.00m, "USD");
 
         var paymentProcessor = new PayPalAdapter(new OldPayPalClient());
-        var paymentRequest = new PaymentRequest("4111111111111111", 100.00m, "USD");
         var paymentResult = await paymentProcessor.ProcessAsync(paymentRequest);
         Console.WriteLine($"Payment Success: {paymentResult.Success}, Transaction ID: {paymentResult.TransactionId}, Message: {paymentResult.Message}");
 
         IPaymentProcessor visaClient = new VisaPaymentClient();
-        var visaPaymentRequest = new PaymentRequest("5500000000000004", 50.00m, "USD");
-        var visaPaymentResult = await visaClient.ProcessAsync(visaPaymentRequest);
+        var visaPaymentResult = await visaClient.ProcessAsync(paymentRequest);
         Console.WriteLine($"Payment Success: {visaPaymentResult.Success}, Transaction ID: {visaPaymentResult.TransactionId}, Message: {visaPaymentResult.Message}");
+
+
+
+        //  to Make more abstructor we can combine this with Creational Pattern - Factory Method
+        Console.WriteLine("-----------------------");
+        Console.WriteLine("Using Factory to create Payment Processor instances");
+        IPaymentProcessor factory = PaymentProcessorFactory.CreateProcessor("PayPal");
+        var factoryPaymentResult = await factory.ProcessAsync(paymentRequest);
+        Console.WriteLine($"Payment Success: {factoryPaymentResult.Success}, Transaction ID: {factoryPaymentResult.TransactionId}, Message: {factoryPaymentResult.Message}");
+
+
 
     }
 }
---
+++ Pattern.Structural/Adapter/PaymentProcessorFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Pattern.Structural.Adapter;
+
+public static class PaymentProcessorFactory
+{
+    public static IPaymentProcessor CreateProcessor(string provider)
+    {
+        return provider.ToLower() switch
+        {
+            "paypal" => new PayPalAdapter(new OldPayPalClient()),
+            "visa" => new VisaPaymentClient(),
+            _ => throw new ArgumentException($"Unknown provider {provider}")
+        };
+    }
+
+
+}
